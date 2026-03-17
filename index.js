1765d14b56adfe957ee4f22748a5a0a81904ef00
Code-God-2010 <linlagode@gmail.com>
2026-03-17 19:21:08 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const iframe = document.getElementById("yt");
   iframe.src += "&autoplay=1";
+  iframe.src -= "&mute=1";
+  iframe.src += "&mute=0";
 }, {once: true});
